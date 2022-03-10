e2cd0084bf51d22b475a476ffbc16d7deb528221
bobby williams <bobby.james.williams@outlook.com>
2022-03-10 22:24:02 +0000

    Generated and configured project. Began laying out functions.

---
+++ src/TCP.java
@@ -0,0 +1,45 @@
+public class TCP {
+    public static void main(String[] args) {
+        System.out.print("hello");
+    }
+
+    private void readTestSuite(){
+
+    }
+
+    private void parseTestSuite(){
+
+    }
+
+    private void calculateStringDistance(){
+
+    }
+
+    private void prioritiseTestCases(){
+
+    }
+
+    private void manhattanDistance(){
+
+    }
+
+    private void euclideanDistance(){
+
+    }
+
+    private void hammingDistance(){
+
+    }
+
+    private void fourthDistance(){
+
+    }
+
+    private void generateRandomOrdering(){
+
+    }
+
+    private void evaluateTestSuite(){
+
+    }
+}
