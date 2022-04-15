7f0e0edfe59998f881b33dc6e17e4613205951fb
Bobby Williams <bobby.james.williams@outlook.com>
2022-04-15 15:53:29 +0100

    averageScore no longer needed with new implementation.

--- src/App/Models/TestCase.java
+++ src/App/Models/TestCase.java
@@ -6,20 +6,12 @@ public class TestCase {
     private final int testID;
     private final String testData;
     private int order;
-    private double averageScore;
-
     public TestCase(int testID, String testData){
         this.testID = testID;
         this.testData = testData;
         this.order = testID;
     }
-
-    public TestCase(int testID, String testData, double averageScore){
-        this.testID = testID;
-        this.testData = testData;
-        this.order = testID;
-        this.averageScore = averageScore;
-    }
+    
 
     public int getTestID(){
         return  testID;
@@ -41,7 +33,7 @@ public class TestCase {
         this.order = order;
     }
 
-    public void setAverageScore(int averageScore){
+    public void setAverageScore(double averageScore){
         this.averageScore = averageScore;
     }
 }
